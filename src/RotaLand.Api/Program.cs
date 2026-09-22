951877133be1a82228b1ffd9bf3ad8c1d924808c
Alexander Iskandar <alexanderokpere@gmail.com>
2026-09-22 23:37:53 +0100

    Allow local mobile frontend access

--- src/RotaLand.Api/Program.cs
+++ src/RotaLand.Api/Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "http://localhost:5175"
+                "http://localhost:5175",
+                "http://172.20.10.4:5175"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
